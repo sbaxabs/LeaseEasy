5346621a39a9e323ac878d38a8c28286c43bff9d
sbaxabs <shirazbaxamusa@gmail.com>
2023-11-04 13:25:07 -0500

    smile

--- smile_scraper.py
+++ smile_scraper.py
@@ -1,1 +1,8 @@
 # For biggie bert!
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+}
+######SMILE TIMEEEEEEEEEEE
+smile_url = "https://www.smilestudentliving.com/availability"
+smile_response = requests.get(smile_url, headers=headers)

6c2505568ea5730286ddc7ebd3e603e1822c29e2
sbaxabs <shirazbaxamusa@gmail.com>
2023-11-04 15:54:09 -0500

    init for berto

--- smile_scraper.py
+++ smile_scraper.py
@@ -1,4 +1,7 @@
 # For biggie bert!
+import requests
+import json
+from bs4 import BeautifulSoup
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
@@ -6,3 +9,42 @@ headers = {
 ######SMILE TIMEEEEEEEEEEE
 smile_url = "https://www.smilestudentliving.com/availability"
 smile_response = requests.get(smile_url, headers=headers)
+print(smile_response.status_code)
+smile_html_content = smile_response.content
+apt_soup = BeautifulSoup(smile_html_content, "html.parser")
+print(smile_html_content)
+# Find all the apartment buildings
+building_cards = apt_soup.find_all("div", class_="listing-item")
+smile_locations = []
+print(building_cards)
+
+# Iterate through apartment building h2
+for card in building_cards:
+
+    # Getting HTML response of each of these buildings
+    location_element = card.find("a").find("slider-link")
+    location = location_element.text.strip()    
+    href = location_element["href"]
+    location_url = f"https://www.smilestudentliving.com{href}"
+
+    # Get HTML response and content
+    location_response = requests.get(location_url)
+    location_html_content = location_response.content
+    location_soup = BeautifulSoup(location_html_content, "html.parser")
+    location_name = location_element["aria-label"]
+
+    # Initialize list of bedroom number
+    bed_numbers = location_soup.find("div", class_= "feature beds")
+    bed_number = bed_numbers.text.strip() 
+    
+
+    # Initialize the list of prices
+    prices = location_soup.find("div", class_= "rent-info")
+    price = prices.text.strip()
+
+    # Initialize the list of bathrooms
+    bathroom_number = location_soup.find("div", class_= "feature baths")
+    
+    room_info = [location_name , bed_number, price, bathroom_number]
+    print(room_info)
+    smile_locations.append(room_info)
--- ugroup_scraper.py
+++ ugroup_scraper.py
@@ -22,34 +22,30 @@ for card in ugroup_cards:
     print(location)   
     detail_element = card.find("div", class_="col-lg-6 propert_sm_detail").find("a")
     href = detail_element["href"]
-    location_url = {href}
-    print(location_url)
+    location_url = href
     ugroup_locations.append(location_url)
+
 
     # Get HTML response and content
     location_response = requests.get(location_url, headers={"User-Agent": "XY"})
+    print(location_response.status_code)
     location_html_content = location_response.content
     location_soup = BeautifulSoup(location_html_content, "html.parser")
-
-    # Initialize dictionary that stores a list of Room bedroom types
-    room_data = {}
-
-    # Initialize the array to be in the dict
-    floorplan = []
-
+    
     # Loop through bedroom tabs
     room_elements = location_soup.find_all("div", class_="tab-content_in_wrapp tab-cntnt_wrap_btm")
-    for parent_div in room_elements:
-        room_divs = parent_div.find_all('div', class_=lambda x: x and "unit__bedrooms-button js" in x and 'js-' in x)
-
-        # room_divs is a list of bed types
-        # Loop through and append text to floorplan array
-        # Make room_data hold location as the key and the floorplan array as the value
-        # Append room_data dictionary to jsm_locations list
-        for room_div in room_divs:
-            floorplan.append(room_div.text)
-    room_data = {location:floorplan}
-    ugroup_locations.append(room_data)
+    for layout in room_elements:
+        info = layout.find_all("div", class_="col-lg-5")
+        room = layout.find("div", class_="col-lg-6 tab-content_in_lft popup-gallery").find("h4")
+        room_numba = room.text.strip()
+        li_list = []
+        for poop in info:
+            poopy = poop.text.strip()
+            li_list.append(poopy)
+        room_data = [room_numba,li_list[1],li_list[7]]
+        print(room_data)
+        ugroup_locations.append([location,room_data])
+    
 
 
 # Items to consider
@@ -60,9 +56,39 @@ for card in ugroup_cards:
 # link
 # Square footage?
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-}
-######UGROUP TIMEEEEEEEEEEE
-ugroup_url = "https://www.smilestudentliving.com/availability"
-ugroup_response = requests.get(ugroup_url, headers=headers)
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+# }
+# ######UGROUP TIMEEEEEEEEEEE
+# ugroup_url = "https://www.smilestudentliving.com/availability"
+# ugroup_response = requests.get(ugroup_url, headers=headers)
+
+# location_url = "https://ugroupcu.com/property-details/303-e-chalmers-january-2024/"
+
+# location_response = requests.get(location_url, headers={"User-Agent": "XY"})
+# location_html_content = location_response.content
+# location_soup = BeautifulSoup(location_html_content, "html.parser")
+
+# # Initialize dictionary that stores a list of Room bedroom types
+# room_data = {}
+
+# # Initialize the array to be in the dict
+# floorplan = []
+
+# # Loop through bedroom tabs
+# room_elements = location_soup.find_all("div", class_="tab-content_in_wrapp tab-cntnt_wrap_btm")
+# for layout in room_elements:
+#     info = layout.find_all("div", class_="col-lg-5")
+#     li_list = []
+#     for poop in info:
+#         poopy = poop.text.strip()
+#         li_list.append(poopy)
+
+#     # room_divs is a list of bed types
+#     # Loop through and append text to floorplan array
+#     # Make room_data hold location as the key and the floorplan array as the value
+#     # Append room_data dictionary to jsm_locations list
+# #     for room_div in room_divs:
+# #         floorplan.append(room_div.text)
+# # room_data = {location:floorplan}
+# # ugroup_locations.append(room_data)

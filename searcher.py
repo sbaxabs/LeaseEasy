2d8be178f781af9bacdaf34838ed87cd9776d0e6
sbaxabs <shirazbaxamusa@gmail.com>
2023-11-04 22:35:02 -0500

    stuff

--- searcher.py
+++ searcher.py
@@ -8,7 +8,25 @@ app.static_url_path = '/static'  # URL path for static files
 def home():
     return render_template("index.html")
 
-
-
 if __name__ == "__main__":
     app.run()
+
+# from flask import Flask, render_template, jsonify
+
+# app = Flask(__name__)
+# app.static_folder = 'static'
+# app.static_url_path = '/static'
+
+# # Include your scraping code here to get the ugroup_json_data
+
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
+# @app.route("/apartments")
+# def get_apartments():
+#     # Return the JSON data from the scraper
+#     return jsonify(ugroup_json_data)
+
+# if __name__ == "__main__":
+#     app.run()
